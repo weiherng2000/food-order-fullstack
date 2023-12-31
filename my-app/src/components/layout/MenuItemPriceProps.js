5dd148c575472d0430b33c85f2fc34325467bf6d
Tan wei herng <weiherng2000@gmail.com>
2023-12-31 17:23:35 +0800

    small update

--- my-app/src/components/layout/MenuItemPriceProps.js
+++ my-app/src/components/layout/MenuItemPriceProps.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import ChevronDown from "../icons/ChevronDown";
 import Plus from "../icons/Plus";
 import Trash from "../icons/Trash";
+import ChevronUp from "../icons/ChevronUp";
 
 export default function MenuItemPriceProps({name,addLabel,props,setProps}){
+
+    const [isOpen,setIsOpen] = useState(false);
     
 
     
@@ -34,28 +39,43 @@ export default function MenuItemPriceProps({name,addLabel,props,setProps}){
 
     return(
         <div className="bg-gray-200 p-2 rounded-md mb-2">
-                     <label>{name}</label>
-                     {props?.length > 0 && props.map( (size,index) => 
-                        <div className="flex items-end gap-2">
-                            <div>
-                                <label>Name</label>
-                               <input type = "text" placeholder="Size name" value = {size.name} onChange={ev => editProp(ev,index,'name')}/>
-                            </div>
-                            <div>
-                                <label>Extra price</label>
-                               <input type = "text" placeholder="Extra price" value = {size.price} onChange={ev =>editProp(ev,index,'price')} />
-                            </div>
-                            <div>
-                                <button type = "button" onClick = {() => removeProp(index)}className="bg-white mb-2 px-2">
-                                    <Trash/>
-                                </button>
-                            </div>
-                        </div>)}
-                     <button type = "button" onClick= {addProp}className="bg-white items-center">
-                        <Plus className="w-4 h-4"/>
-
-                         <span>{addLabel} </span>
+                    
+                    {/*button toggles here so setIsOpen will always be the opposite value of the prev value */}
+                    <button onClick = { () => setIsOpen(prev => !prev)}type = "button" className="inline-flex p-1 border-0 justify-start">
+                        {isOpen && (
+                            <ChevronUp/>
+                        )}
+                        {!isOpen && (
+                            <ChevronDown/>
+                        )}
+                        <span>{name}</span>
+                        <span>({props?.length})</span>
                     </button>
+                    {/*div block is hidden until we click the button to open it up */}
+                    <div className= {isOpen ? 'block' : 'hidden'}>
+                        {props?.length > 0 && props.map( (size,index) => 
+                            <div className="flex items-end gap-2">
+                                <div>
+                                    <label>Name</label>
+                                <input type = "text" placeholder="Size name" value = {size.name} onChange={ev => editProp(ev,index,'name')}/>
+                                </div>
+                                <div>
+                                    <label>Extra price</label>
+                                <input type = "text" placeholder="Extra price" value = {size.price} onChange={ev =>editProp(ev,index,'price')} />
+                                </div>
+                                <div>
+                                    <button type = "button" onClick = {() => removeProp(index)}className="bg-white mb-2 px-2">
+                                        <Trash/>
+                                    </button>
+                                </div>
+                            </div>)}
+                        <button type = "button" onClick= {addProp}className="bg-white items-center">
+                            <Plus className="w-4 h-4"/>
+
+                            <span>{addLabel} </span>
+                        </button>
+                    </div>
+                     
                 </div>
     );
 }
