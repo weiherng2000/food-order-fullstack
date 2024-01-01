4eac5b65d51b697f8f00fade78021e61ecc043a7
Tan wei herng <weiherng2000@gmail.com>
2024-01-01 16:40:04 +0800

    update

--- my-app/src/app/api/profile/route.js
+++ my-app/src/app/api/profile/route.js
@@ -4,43 +4,48 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "@/app/models/User";
 import { UserInfo } from "@/app/models/UserInfo";
 
-//PUTinformation into the mongo db about our user data
-export async function PUT(req){
-     mongoose.connect(process.env.MONGO_URL);
-     const data = await req.json();
-     const {_id, name, image, ...otherUserInfo} = data;
-     
+export async function PUT(req) {
+  mongoose.connect(process.env.MONGO_URL);
+  const data = await req.json();
+  const {_id, name, image, ...otherUserInfo} = data;
 
-     const session = await getServerSession(authOptions);
-     const email = session.user.email;
-     
-     await User.updateOne({email},{name, image});
+  let filter = {};
+  if (_id) {
+    filter = {_id};
+  } else {
+    const session = await getServerSession(authOptions);
+    const email = session.user.email;
+    filter = {email};
+  }
 
-     await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert:true});
-   
-  
+  const user = await User.findOne(filter);
+  await User.updateOne(filter, {name, image});
+  await UserInfo.findOneAndUpdate({email:user.email}, otherUserInfo, {upsert:true});
 
-     return Response.json('true');
+  return Response.json(true);
 }
 
-//GET information from the mongo db about our user data
 export async function GET(req) {
+  mongoose.connect(process.env.MONGO_URL);
 
-   mongoose.connect(process.env.MONGO_URL);
- 
-   const url = new URL(req.url);
-   const _id = url.searchParams.get('_id');
- 
-   
-   const session = await getServerSession(authOptions);
-   const email = session?.user?.email;
+  const url = new URL(req.url);
+  const _id = url.searchParams.get('_id');
 
-   if(!email){
-     return Response.json({});
-   }
-   const user = await User.findOne({email}).lean();
-   const userInfo = await UserInfo.findOne({email}).lean();
-   
-   return Response.json({...user, ...userInfo});
- 
- }
+  let filterUser = {};
+  if (_id) {
+    filterUser = {_id};
+  } else {
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    if (!email) {
+      return Response.json({});
+    }
+    filterUser = {email};
+  }
+
+  const user = await User.findOne(filterUser).lean();
+  const userInfo = await UserInfo.findOne({email:user.email}).lean();
+
+  return Response.json({...user, ...userInfo});
+
+}
--- my-app/src/app/profile/page.js
+++ my-app/src/app/profile/page.js
@@ -1,135 +1,73 @@
-'use client'
+'use client';
 import EditableImage from "@/components/layout/EditableImage";
 import InfoBox from "@/components/layout/InfoBox";
 import SuccessBox from "@/components/layout/SuccessBox";
+import UserForm from "@/components/layout/UserForm";
 import UserTabs from "@/components/layout/UserTabs";
-import { useSession } from "next-auth/react";
+import {useSession} from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect } from 'next/navigation'
+import {redirect} from "next/navigation";
 import {useEffect, useState} from "react";
 import toast from "react-hot-toast";
 
-export default function ProfilePage(){
-    const session = useSession();
-    const {status} = session;
+export default function ProfilePage() {
+  const session = useSession();
 
-    const [image,setImage] = useState('');
-    const [userName,setUserName] = useState('');
-    const [streetAddress,setStreetAddress] = useState('');
-    const [phone,setPhone] = useState('');
-    const [postalCode,setPostalCode] = useState('');
-    const [city,setCity] = useState('');
-    const [country,setCountry] = useState('');
-    const [isAdmin,setIsAdmin]  = useState(false);
-    const [profileFetched, setProfileFetched] = useState(false);
-   
+  const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [profileFetched, setProfileFetched] = useState(false);
+  const {status} = session;
 
-    useEffect(() => {
-        if (status === 'authenticated') {
-          setUserName(session.data.user.name);
-          setImage(session.data.user.image);
-          //fetch by default GETs info
-          fetch('/api/profile').then(response =>{
-            response.json().then(data =>{
-                setPhone(data.phone);
-                setStreetAddress(data.streetAddress);
-                setPostalCode(data.postalCode);
-                setCity(data.city);
-                setCountry(data.country);
-                setIsAdmin(data.admin);
-                setProfileFetched(true);
-            })
-          });
-        }
-      }, [session, status]);
-    
-
-    async function handleProfileInfoUpdate(ev){
-        ev.preventDefault();
-      
-        const savingPromise = new Promise(async (resolve, reject) => {
-            const response = await fetch('/api/profile', {
-              method: 'PUT',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({name:userName,image,streetAddress,phone,postalCode,city,country}),
-            });
-            if (response.ok)
-              resolve()
-            else
-              reject();
-          });
-          
-          //toast is a react library which allows us to have pop up alert styled boxes
-          await toast.promise(savingPromise, {
-            loading: 'Saving...',
-            success: 'Profile saved!',
-            error: 'Error',
-          });
-        
-       
+  useEffect(() => {
+    if (status === 'authenticated') {
+      fetch('/api/profile').then(response => {
+        response.json().then(data => {
+          setUser(data);
+          setIsAdmin(data.admin);
+          setProfileFetched(true);
+        })
+      });
     }
+  }, [session, status]);
 
-    
-    
-    //returns loading text on the screen when page
-    if(status === 'loading' || !profileFetched)
-    {
-        return 'Loading...';
-    }
-    //redirects user to login page if session status is not authenticated means user havent logged in
-    if(status === 'unauthenticated')
-    {
-        redirect('/login');
-    }
+  async function handleProfileInfoUpdate(ev, data) {
+    ev.preventDefault();
 
- 
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch('/api/profile', {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+      });
+      if (response.ok)
+        resolve()
+      else
+        reject();
+    });
 
-    return(
-        <section className="mt-8">
-            <UserTabs isAdmin={isAdmin}/>
-          
-           
-            <div className="max-w-2xl mx-auto mt-8">
-              
-                
-                <div className="flex gap-4 ">
-                    <div className=" p-2 rounded-lg relative max-w-[120px] max-h-[120px]" >
-                       <EditableImage link = {image} setLink={setImage}/>
-                       
-               
-                    </div>
-                   
-                    <form className="grow" onSubmit = {handleProfileInfoUpdate}>
-                        <label>First and last name</label>
-                        <input type = "text" placeholder="First and last name" value={userName} onChange={ ev => setUserName(ev.target.value)}/>
-                        <label>Email</label>
-                        <input type = "email" value = {session.data.user.email} disabled = {true} placeholder = {'email'}/>
-                        <label>Phone</label>
-                        <input  type = "tel" placeholder="Phone number" value = {phone} onChange = { ev => setPhone(ev.target.value)}/>
-                        <label>Street Address</label>
-                        <input  type = "text" placeholder="Street Address"  value = {streetAddress} onChange = { ev => setStreetAddress(ev.target.value)}/>
-                        <div className="flex gap-2">
-                            <div>
-                                <label>City</label>
-                                <input   type = "text" placeholder="City Address"  value = {city} onChange = { ev => setCity(ev.target.value)}/>
-                            </div>
-                            <div>
-                                <label>Postal Code</label>
-                                <input   type = "text" placeholder="Postal code"  value = {postalCode} onChange = { ev => setPostalCode(ev.target.value)}/>
-                            </div>
-                           
-                        </div>
-                        <label>Country</label>
-                        <input  type = "text" placeholder="Country"  value = {country} onChange = { ev => setCountry(ev.target.value)}/>
-                      
-                        
-                        <button type = "submit">Save</button>
-                    </form>
-                </div>
-            </div> 
+    await toast.promise(savingPromise, {
+      loading: 'Saving...',
+      success: 'Profile saved!',
+      error: 'Error',
+    });
 
-        </section>
-    );
+  }
 
+  if (status === 'loading' || !profileFetched) {
+    return 'Loading...';
+  }
+
+  if (status === 'unauthenticated') {
+    return redirect('/login');
+  }
+
+  return (
+    <section className="mt-8">
+      <UserTabs isAdmin={isAdmin} />
+      <div className="max-w-2xl mx-auto mt-8">
+        <UserForm user={user} onSave={handleProfileInfoUpdate} />
+      </div>
+    </section>
+  );
 }
